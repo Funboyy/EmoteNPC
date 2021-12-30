5fdefa855d7649dfa0b1c30a3d562095e07ae697
Funboyy <72627658+Funboyy@users.noreply.github.com>
2021-12-30 01:11:25 +0100

    changed version handling and added support for versions 1.17-1.18.1

--- src/main/java/de/funboyy/labymod/emote/npc/utils/Versions.java
+++ src/main/java/de/funboyy/labymod/emote/npc/utils/Versions.java
@@ -1,10 +1,29 @@
 package de.funboyy.labymod.emote.npc.utils;
 
+import de.funboyy.labymod.emote.npc.packet.*;
 import lombok.Getter;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 
 public class Versions {
+
+    public static final int v1_8_R1 = 1_8_1;
+    public static final int v1_8_R2 = 1_8_2;
+    public static final int v1_8_R3 = 1_8_3;
+    public static final int v1_9_R1 = 1_9_1;
+    public static final int v1_9_R2 = 1_9_2;
+    public static final int v1_10_R1 = 1_10_1;
+    public static final int v1_11_R1 = 1_11_1;
+    public static final int v1_12_R1 = 1_12_1;
+    public static final int v1_13_R1 = 1_13_1;
+    public static final int v1_13_R2 = 1_13_2;
+    public static final int v1_14_R1 = 1_14_1;
+    public static final int v1_15_R1 = 1_15_1;
+    public static final int v1_16_R1 = 1_16_1;
+    public static final int v1_16_R2 = 1_16_2;
+    public static final int v1_16_R3 = 1_16_3;
+    public static final int v1_17_R1 = 1_17_1;
+    public static final int v1_18_R1 = 1_18_1;
 
     private static Versions instance;
 
@@ -12,8 +31,8 @@ public class Versions {
         try {
             final int versionId = Integer.parseInt(version.replace("_", "")
                     .replace("R", "").substring(1));
-            if (versionId < 181 || versionId > 1165) {
-                throw new RuntimeException("Wrong version");
+            if (versionId < v1_8_R1 || versionId > v1_18_R1) {
+                    throw new RuntimeException("Wrong version");
             }
 
             instance = new Versions(version);
@@ -30,24 +49,28 @@ public class Versions {
     }
 
     @Getter private final String version;
-    @Getter private final int versionId;
+    @Getter private final int id;
 
     private Versions(final String version) {
         this.version = version;
-        this.versionId = Integer.parseInt(version.replace("_", "")
+        this.id = Integer.parseInt(version.replace("_", "")
                 .replace("R", "").substring(1));
     }
 
     public boolean isMinecraft18() {
-        return this.versionId >= 181 && this.versionId <= 183;
+        return this.id >= v1_8_R1 && this.id <= v1_8_R3;
     }
 
     public boolean isMinecraft19() {
-        return this.versionId == 191 || this.versionId == 192;
+        return this.id == v1_9_R1 || this.id == v1_9_R2;
+    }
+
+    public boolean hasNewProtocol() {
+        return this.id >= v1_17_R1;
     }
 
     public Material getSkull() {
-        return Versions.getInstance().getVersionId() <= 1121
+        return this.id <= v1_12_R1
                 ? Material.valueOf("SKULL_ITEM") : Material.valueOf("PLAYER_HEAD");
     }
 
@@ -60,11 +83,15 @@ public class Versions {
             return 6;
         }
 
-        if (this.versionId <= 1132) {
+        if (this.id <= v1_13_R2) {
             return 7;
         }
 
-        return 8;
+        if (this.id <= v1_16_R3) {
+            return 8;
+        }
+
+        return 9;
     }
 
     public int getSkinOverlay() {
@@ -76,15 +103,31 @@ public class Versions {
             return 12;
         }
 
-        if (this.versionId <= 1132) {
+        if (this.id <= v1_13_R2) {
             return 13;
         }
 
-        if (this.versionId <= 1141) {
+        if (this.id <= v1_14_R1) {
             return 15;
         }
 
-        return 16;
+        if (this.id <= v1_16_R3) {
+            return 16;
+        }
+
+        return 17;
+    }
+
+    public Class<? extends IEmoteNPC> getEmoteNPC() {
+        return hasNewProtocol() ? EmoteNPC_1_17to1_18_1.class : EmoteNPC_1_8to1_16_5.class;
+    }
+
+    public Class<? extends IPacketReader> getPacketReader() {
+        return hasNewProtocol() ? PacketReader1_17to1_18_1.class : PacketReader_1_8to1_16_5.class;
+    }
+
+    public Class<? extends IProtocol> getProtocol() {
+        return hasNewProtocol() ? Protocol1_17to1_18_1.class : Protocol_1_8to1_16_5.class;
     }
 
 }
