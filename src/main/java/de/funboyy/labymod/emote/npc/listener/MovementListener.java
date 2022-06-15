e61a64f5c0212d1f3d6980fe261efce54d4e021e
Funboyy <72627658+Funboyy@users.noreply.github.com>
2022-06-15 13:47:53 +0200

    fixed error when sneaking while npc is not loaded and handel teleporting as movement

--- src/main/java/de/funboyy/labymod/emote/npc/listener/MovementListener.java
+++ src/main/java/de/funboyy/labymod/emote/npc/listener/MovementListener.java
@@ -10,11 +10,13 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 
 public class MovementListener implements Listener {
 
     @EventHandler
+    @SuppressWarnings("unused")
     public void onChange(final PlayerChangedWorldEvent event) {
         final User user = UserManager.getInstance().getUser(event.getPlayer());
 
@@ -22,7 +24,7 @@ public class MovementListener implements Listener {
             return;
         }
 
-        Bukkit.getScheduler().runTaskLater(EmoteNPCPlugin.getInstance(), ()-> {
+        Bukkit.getScheduler().runTaskLaterAsynchronously(EmoteNPCPlugin.getInstance(), ()-> {
             if (!user.isNearNPC()) {
                 if (!user.getNpc().isSpawned()) {
                     return;
@@ -39,6 +41,7 @@ public class MovementListener implements Listener {
     }
 
     @EventHandler
+    @SuppressWarnings("unused")
     public void onMove(final PlayerMoveEvent event) {
         final User user = UserManager.getInstance().getUser(event.getPlayer());
 
@@ -69,6 +72,7 @@ public class MovementListener implements Listener {
     }
 
     @EventHandler
+    @SuppressWarnings("unused")
     public void onSneak(final PlayerToggleSneakEvent event) {
         final User user = UserManager.getInstance().getUser(event.getPlayer());
 
@@ -80,11 +84,44 @@ public class MovementListener implements Listener {
             return;
         }
 
+        if (!user.getNpc().isSpawned()) {
+            user.getNpc().spawn();
+        }
+
         if (!Config.getInstance().sneak()) {
             return;
         }
 
         user.getNpc().sneak(event.isSneaking());
+    }
+
+    @EventHandler
+    @SuppressWarnings("unused")
+    public void onTeleport(final PlayerTeleportEvent event) {
+        final User user = UserManager.getInstance().getUser(event.getPlayer());
+
+        if (user == null) {
+            return;
+        }
+
+        Bukkit.getScheduler().runTaskLaterAsynchronously(EmoteNPCPlugin.getInstance(), ()-> {
+            if (!user.isNearNPC()) {
+                if (!user.getNpc().isSpawned()) {
+                    return;
+                }
+
+                user.getNpc().remove();
+                return;
+            }
+
+            if (!user.getNpc().isSpawned()) {
+                user.getNpc().spawn();
+            } else if (Config.getInstance().lookClose()) {
+                final Location location = user.getPlayer().getLocation();
+                final Location npcLocation = location.setDirection(location.subtract(Config.getInstance().getLocation()).toVector());
+                user.getNpc().headRotation(npcLocation.getYaw(), npcLocation.getPitch());
+            }
+        }, 1);
     }
 
 }
